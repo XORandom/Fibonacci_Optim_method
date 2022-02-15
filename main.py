ba1c87fd9d55c5c24d8672f2805999707dd50698
XORandom <40043238+XORandom@users.noreply.github.com>
2022-02-16 01:04:01 +0300

    Final
    Поиск минимума функции одной переменной. Используются методы Фибоначчи и золотого сечения

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 
 
 def func_task(x):
@@ -8,16 +8,39 @@ def func_task(x):
 
 
 # Поиск последовательности Фибоначчи
-def fib(input_num):
-    a = int(input_num)
+def fib(input_num, show=0):
+    n = int(input_num)
     fb = [1, 1]
-    for x in range(2, a):
-        fb.append(fb[x - 2] + fb[x - 1])
-    return fb[input_num-1]
+    for k in range(2, n):
+        fb.append(fb[k - 2] + fb[k - 1])
+    if show == 1:
+        print('Последовательность Фибоначчи', '[', input_num, ']', ': ', fb)
+    else:
+        return fb[input_num-1]
 
 
-def fibonacci(a, b, eps, N=10):
-    print("Поиск методом Фибоначчи")
+
+def fibonacci(a, b, eps, N):
+    a0 = a
+    b0 = b
+    k = 0
+    y0 = a0+((fib(N-2)/fib(N))*(b0-a0))
+    z0 = a0+((fib(N-1)/fib(N))*(b0-a0))
+    while abs(b0 - a0) > eps:
+        if func_task(y0) > func_task(z0):
+            a0 = y0
+            y0 = z0
+            z0 = a0+((fib(N-k-2)/fib(N-k-1))*(b0-a0))
+        else:
+            b0=z0
+            z0=y0
+            y0=a0+((fib(N-k-3)/fib(N-k-1))*(b0-a0))
+        if ++k > 10e5:
+            break
+    return (a0 + b0) / 2
+
+
+def gold(a, b, eps):
     g = (math.sqrt(5) - 1.0) / 2
     k = 0
     a1 = a + (1 - g) * (b - a)
@@ -31,7 +54,7 @@ def fibonacci(a, b, eps, N=10):
             b = b1
             b1 = a1
             a1 = a + (1 - g) * (b - a)
-        if ++k > N:
+        if ++k > 10e5:
             break
     return (a + b) / 2
 
@@ -40,14 +63,21 @@ if __name__ == '__main__':
     print('e = ', math.e)
     a = 0
     b = 1
-    print(fib(0))
+    N = 10
     eps = 0.000001
-    x = fibonacci(a, b, eps)
+
+    x = fibonacci(a, b, eps, N)
+    fx = func_task(x)
+    print("Поиск методом Фибоначчи")
+    fib(N, 1)
+    print('x =', round(x, 6))
+    print('f(x) =', round(fx, 6))
+
+    x = gold(a, b, eps)
+    print("Повысим точность")
     fx = func_task(x)
     print('x =', x)
     print('f(x) = ', fx)
-    print('округление до 6 знака')
-    print('x =', round(x, 6))
-    print('f(x) =', round(fx, 6))
+
 
 
